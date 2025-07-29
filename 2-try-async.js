8cb85c8dc0335844e21c1cc3c0ab05802df836c2
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-07-29 13:16:14 +0800

    all-the-fetch: Using the Async - await with try-catch method.

--- 2-try-async.js
+++ 2-try-async.js
@@ -1,5 +1,15 @@
 // using async and await instead of chaining then()
 // still needs error handling with try..catch
-const url = "https://jsonplaceholder.typicode.com/usersdfdfgdf";
+const url = "https://jsonplaceholder.typicode.com/users";
 
-export async function getData() {}
+export async function getData() {
+  try {
+    const response = await fetch(url);
+    console.log(response);
+    if (!response.ok) throw new Error("Not valid response");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(`Error: `, error.message);
+  }
+}
