2c60a63a2af94247855bcd85a0f991a54802c817
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-07-29 13:33:59 +0800

    all-the-fetch : Using the url-request .

--- 3-url-request.js
+++ 3-url-request.js
@@ -17,4 +17,27 @@ const str = "http://127.0.0.1:5500/local-sample.json?attempt=123&other=hello";
 
 export function getData() {
   //
+  const url = new URL(str);
+  console.log(`URL HOST`, url.host);
+  console.log(`URL ORIGIN`, url.origin);
+  console.log(`URL PROTOCOL`, url.protocol);
+  console.log(`URL PORT`, url.port);
+  console.log(`URL PATHNAME`, url.pathname);
+
+  const request = new Request(url, {
+    headers: { "x-ray": "Hi" },
+    method: "GET",
+    cache: "no-store",
+  });
+
+  fetch(request)
+    .then((response) => {
+      console.log(response.status);
+      if (!response.ok) throw new Error("Not valid response");
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => console.warn(error.message));
 }
--- main.js
+++ main.js
@@ -52,8 +52,8 @@ Bonus: Measuring Download progress
 **/
 
 // import { getData } from "./1-basic-fetch.js";
-import { getData } from "./2-try-async.js";
-// import { getData } from './3-url-request.js';
+// import { getData } from "./2-try-async.js";
+import { getData } from "./3-url-request.js";
 // import { getData } from './4-response.js';
 // import { getData } from './5-generate.js';
 // import { getData } from './6-auth.js';
