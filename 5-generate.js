a515d22da52c2375ff37148c93734e5868ca5e01
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-07-29 19:44:39 +0800

    all-the-fetch: Implementing the json, text and blob methods.

--- 5-generate.js
+++ 5-generate.js
@@ -1,7 +1,42 @@
 // Create Webpage content from fetch results
 
-const jsonstr = "https://random-data-api.com/api/v2/users?size=10";
+const jsonstr = "https://jsonplaceholder.typicode.com/todos?_limit=10";
 const imgstr = "https://picsum.photos/id/237/300/200";
-const textstr = "http://127.0.0.1:3000/";
+// const textstr = "http://127.0.0.1:3000/";
+const textstr = "https://jsonplaceholder.typicode.com/comments/1";
 
-export function getData() {}
+export function getData() {
+  const img = document.querySelector("#pic");
+  let header = document.querySelector("header");
+
+  fetch(jsonstr)
+    .then((response) => {
+      if (!response.ok) throw new Error("Invalid response");
+      return response.json();
+    })
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error.message));
+
+  fetch(textstr)
+    .then((response) => {
+      if (!response.ok) throw new Error("Invalid response");
+      console.log(response.status);
+      return response.text();
+    })
+    .then((txt) => {
+      header.innerHTML += `<h2>${txt}</h2>`;
+    })
+    .catch((error) => console.warn(error.message));
+
+  fetch(imgstr)
+    .then((response) => {
+      if (!response.ok) throw new Error("Invalid Response");
+      return response.blob();
+    })
+    .then((blob) => {
+      console.log(blob);
+      const url = URL.createObjectURL(blob);
+      img.src = url;
+    })
+    .catch((error) => console.warn(error.message));
+}
