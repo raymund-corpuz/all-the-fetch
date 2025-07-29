87056b693204decc0ea389419bedd8ef8629e7ed
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-07-29 21:58:53 +0800

    all-the-fetch: Uploading files.

--- 7-upload.js
+++ 7-upload.js
@@ -6,4 +6,40 @@ let endpoint = "http://127.0.0.1:3000/";
 
 export function setData() {
   //
+  const imgInput = document.getElementById("imgfile");
+  const jsonInput = document.getElementById("jsonfile");
+  document.getElementById("myform").addEventListener("submit", (ev) => {
+    ev.preventDefault();
+
+    let obj = {
+      id: 123,
+      name: "raymund",
+    };
+
+    let jsonstring = JSON.stringify(obj);
+
+    let fd = new FormData();
+    console.log(imgInput.files);
+    fd.append("imageFile", imgInput.files[0], imgInput.files[0].name);
+
+    let request = new Request(endpoint, {
+      method: "POST",
+      // body: jsonstring,
+      body: fd,
+      headers: {
+        // "content-type": "application/json",
+        "content-type": "multipart/form-data",
+      },
+    });
+
+    fetch(request)
+      .then((response) => {
+        if (!response.ok) throw new Error("invalid response");
+        return response.text();
+      })
+      .then((txt) => {
+        console.log(txt);
+      })
+      .catch((error) => console.error(error.message));
+  });
 }
